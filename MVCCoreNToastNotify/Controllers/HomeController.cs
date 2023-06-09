237cbe71ed60488943e815a039aac18d2dc8403e
karaoz.onr@gmail.com <karaoz.onr@gmail.com>
2023-06-09 09:44:20 +0300

    Add project files.

---
+++ MVCCoreNToastNotify/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCCoreNToastNotify.Models;
+using NToastNotify;
+using System.Diagnostics;
+
+namespace MVCCoreNToastNotify.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IToastNotification _notification;  
+        public HomeController(ILogger<HomeController> logger,IToastNotification toastNotification)
+        {
+            _logger = logger;
+            _notification = toastNotification;
+        }
+
+        public IActionResult Index()
+        {
+            _notification.AddSuccessToastMessage(message:"Merhaba Dünya la");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVCCoreNToastNotify/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCCoreNToastNotify.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
